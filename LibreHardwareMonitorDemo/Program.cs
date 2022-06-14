b62c81048586697c73b8af20e7e644440e22137a
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-06-14 12:05:26 +0300

    Added LibreHardwareMonitorDemo

---
+++ LibreHardwareMonitorDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+using LibreHardwareMonitor.Hardware;
+using LibreHardwareMonitorDemo;
+
+Console.WriteLine("LibreHardwareMonitor Demo.");
+Console.WriteLine("For more info visit this url: https://github.com/LibreHardwareMonitor/LibreHardwareMonitor");
+Console.WriteLine();
+
+int menu = GetConsoleMenu();
+while (menu > 0)
+{
+    Console.WriteLine();
+    switch (menu)
+    {
+        case 1:
+            Monitor();
+            break;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+    Console.Clear();
+    menu = GetConsoleMenu();
+}
+
+int GetConsoleMenu()
+{
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Run Demo");
+    Console.Write("Enter your choice: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Monitor()
+{
+    Computer computer = new()
+    {
+        IsCpuEnabled = true,
+        IsGpuEnabled = true,
+        IsMemoryEnabled = true,
+        IsMotherboardEnabled = true,
+        IsControllerEnabled = true,
+        IsNetworkEnabled = true,
+        IsStorageEnabled = true
+    };
+    computer.Open();
+    computer.Accept(new UpdateVisitor());
+
+    foreach (IHardware hardware in computer.Hardware)
+    {
+        Console.WriteLine("Hardware: {0}", hardware.Name);
+        foreach (IHardware subhardware in hardware.SubHardware)
+        {
+            Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+            foreach (ISensor sensor in subhardware.Sensors)
+            {
+                Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+            }
+        }
+        foreach (ISensor sensor in hardware.Sensors)
+        {
+            Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+        }
+    }
+
+    computer.Close();
+}
---
+++ LibreHardwareMonitorDemo/UpdateVisitor.cs
@@ -0,0 +1,22 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using LibreHardwareMonitor.Hardware;
+
+namespace LibreHardwareMonitorDemo
+{
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+}
