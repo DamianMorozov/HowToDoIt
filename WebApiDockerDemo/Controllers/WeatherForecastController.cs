a5756db828fd6b5cc66a13e62a06d5f4e8b3812e
Damian Viktorovich Morozov <damian@morozov33.ru>
2025-03-22 15:12:28 +0300

    Updated Directory.Build

--- WebApiDockerDemo/Controllers/WeatherForecastController.cs
+++ WebApiDockerDemo/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ namespace WebApiDockerDemo.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+public class WeatherForecastController() : ControllerBase
 {
 	private static readonly string[] Summaries = {
 		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
